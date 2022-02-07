1e2203bc136c99c783d05fb827f277ef8675e349
unnamed <arrynpal@ox-u1.msk.21-school.ru>
2022-02-07 15:41:58 +0300

    maxmin

--- src/maxmin.c
+++ src/maxmin.c
@@ -2,39 +2,37 @@
 
 void maxmin(int prob1, int prob2, int prob3, int *max, int *min);
 
-int main()
-{
-    int x, y, z;
-    if(!scanf("%d %d %d", &x, &y, &z)) {
-        printf("n/a");
-        return -1;
-    }
-    if ((int) x  != x || (int) y  != y || (int) z  != z) {
-        printf("n/a");
-        return -1;
-    }
-
-    int max, min;
-
-    maxmin(x, y, z, &max, &min);
-
-    printf("%d %d", max, min);
-
-    return 0;
+int main() {
+int x, y, z;
+if(!scanf("%d %d %d", &x, &y, &z)) {
+printf("n/a");
+return -1;
+}
+if ((int) x  != x || (int) y  != y || (int) z  != z) {
+printf("n/a");
+return -1;
 }
 
-void maxmin(int prob1, int prob2, int prob3, int *max, int *min)
-{
-    *max = prob1;
-    *min = prob1;
-    
-    if(prob2 > *max)
-        *max = prob2;
-    if(prob2 < *min)
-        *min = prob2;
-    
-    if(prob3 > *max)
-        *max = prob3;
-    if(prob3 < *min)
-        *min = prob3;    
+int max, min;
+
+maxmin(x, y, z, &max, &min);
+
+printf("%d %d", max, min);
+
+return 0;
+}
+
+void maxmin(int prob1, int prob2, int prob3, int *max, int *min) {
+*max = prob1;
+*min = prob1;
+
+if(prob2 > *max)
+*max = prob2;
+if(prob2 < *min)
+*min = prob2;
+
+if(prob3 > *max)
+*max = prob3;
+if(prob3 < *min)
+*min = prob3;
 }

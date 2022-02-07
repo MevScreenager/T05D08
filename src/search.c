5227e47a6e9ace584111e315d606f956bfbf99a1
unnamed <arrynpal@ox-u1.msk.21-school.ru>
2022-02-07 22:00:22 +0300

    search.c

--- src/search.c
+++ src/search.c
@@ -1,16 +1,58 @@
-/*
-    Search module for the desired value from data array.
+#include <stdio.h>
+#include <math.h>
+#define NMAX 30
 
-    Returned value must be:
-        - "even"
-        - ">= mean"
-        - "<= mean + 3 * sqrt(variance)"
-        - "!= 0"
+int input(int *a, int *n);
+void output(int *a, int *n, double mean_v);
+int rulse(int *a, int *n, int n); // дописать
+double mean(int *a, int *n);
+double root_mean_square(int *a, int *n);
 
-        OR
+int main() {
+    int n, data[NMAX];
+    if (input(data, &n)) output(data, &n, mean(data, &n));
+    else printf("n/a");
+    return 0;
+}
 
-        0
-*/
+int rulse(int *a, int *n, int np) {
+    double mean_v = mean(a, n), down, up, root_mean_square_v = root_mean_square(int a, int n);
+    down = mean_v - 3 * root_mean_square_v;
+    up = mean_v + 3 * root_mean_square_v;
+    if (np >= down || np <= up) return 1;
+    return 0;
+}
 
+double root_mean_square(int *a, int *n) {
+    double mean_v = mean(a, n), sum = 0;
+    for(int *p = a; p - a < *n; p++) 
+        sum = sum + pow(*p - mean_v, 2);
+    return sqrt((sum * 1.0) / *n);
+}
 
+double mean(int *a, int *n) {
+    int sum = 0;
+    for (int *p = a; p - a < *n; p++) sum = sum + *p;
+    return (sum * 1.0) / *n;
+}
 
+int input(int *a, int *n) {
+    if (!scanf("%d", n)) return 0;
+    if ((int *) n  != n) return 0;
+
+    for(int *p = a; p - a < *n; p++) {
+        if (!scanf("%d", p)) return 0;
+        if ((int *) p  != p) return 0;
+    }
+    return 1;
+}
+
+void output(int *a, int *n, double mean_v) {
+    for(int *p = a; p - a < *n; p++) {
+        if (*p % 2 == 0 && (*p > mean_v || *p == mean_v) && rulse(a, n, *p) && *p != 0) {
+            printf("%d", *p);
+            return;
+        }
+    }
+    printf("%d", 0);
+}

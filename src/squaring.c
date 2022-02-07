141bac9987cf57f01dce20a355c4c20d5911c4a3
unnamed <arrynpal@ox-u1.msk.21-school.ru>
2022-02-07 20:15:39 +0300

    squaring.c

--- src/squaring.c
+++ src/squaring.c
@@ -2,36 +2,35 @@
 #define NMAX 10
 
 int input(int *a, int *n);
-void output(int *a, int n);
-void squaring(int *a, int n);
+void output(int *a, int *n);
+void squaring(int *a, int *n);
 
-int main()
-{
-    int n, data[NMAX];
-    input(data, n);
-    squaring(data, n);
-    output(data, n);
-
-    return 0;
+int main() {
+int n, data[NMAX];
+if (input(data, &n)) {
+squaring(data, &n);
+output(data, &n);
+} else {
+printf("n/a");
+}
+return 0;
 }
 
-int input(int *a, int *n)
-{
-    scanf("%d", n);
-    for(int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
-    }
+int input(int *a, int *n) {
+if (!scanf("%d", n)) return 0;
+if ((int *) n  != n) return 0;
+
+for(int *p = a; p - a < *n; p++) {
+if (!scanf("%d", p)) return 0;
+if ((int *) p  != p) return 0;
+}
+return 1;
 }
 
-void output(int *a, int n)
-{
-    //NOTHING
+void squaring(int *a, int *n) {
+for(int *p = &a[0]; p - a < *n; p++) *p = *p * *p;
 }
 
-void squaring(int *a, int n)
-{
-    //NOTHING
+void output(int *a, int *n) {
+for(int *p = a; p - a < *n; p++) printf("%d ", *p);
 }
-
-

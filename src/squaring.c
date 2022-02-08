27e17b4b46b8f1c90badd2e0e1568255bc77ba39
unnamed <arrynpal@ox-h5.msk.21-school.ru>
2022-02-08 14:20:08 +0300

    squaring.c

--- src/squaring.c
+++ src/squaring.c
@@ -20,7 +20,7 @@ int input(int *a, int *n) {
     if (!scanf("%d", n)) return 0;
     if ((int *) n  != n) return 0;
 
-    for(int *p = a; p - a < *n; p++) {
+    for (int *p = a; p - a < *n; p++) {
         if (!scanf("%d", p)) return 0;
         if ((int *) p  != p) return 0;
     }
@@ -29,11 +29,11 @@ int input(int *a, int *n) {
 }
 
 void squaring(int *a, int *n) {
-    for(int *p = &a[0]; p - a < *n; p++) *p = *p * *p;
+    for (int *p = &a[0]; p - a < *n; p++) *p = *p * *p;
 }
 
 void output(int *a, int *n) {
-    for(int *p = a; p - a < *n; p++){
+    for (int *p = a; p - a < *n; p++) {
         if (p - a == *n - 1) {
             printf("%d", *p);
             return;
